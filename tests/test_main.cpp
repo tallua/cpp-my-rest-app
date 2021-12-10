41dd9c2ca282bb172e6cb04dcbd701b9a6175280
tallua <talluay@gmail.com>
2021-12-10 18:18:22 +0000

    add gtest to build system

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <gtest/gtest.h>
 
-int main(int argc, char** argv)
-{
-    std::cout << "Hello, Test" << std::endl;
+TEST(Hello, Test) {
+    EXPECT_EQ(3, 3);
+}
 
-    return 0;
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
