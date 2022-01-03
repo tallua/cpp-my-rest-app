9129094b7f1582f9edc90480c24c3a839a0ccad3
tallua <talluay@gmail.com>
2022-01-03 10:26:49 +0000

    change port of each test

--- tests/test_core.cpp
+++ tests/test_core.cpp
@@ -38,11 +38,11 @@ TEST(RestServer, test_get_p)
 {
     const std::string expect_response("response of test_get");
 
-    RestServer server(U("http://0.0.0.0:1234"));
+    RestServer server(U("http://0.0.0.0:1001"));
     server.OnGet("/test_url", [&](auto req) { return expect_response; });
     server.Run();
 
-    const auto actual_response = request_get("http://0.0.0.0:1234", "/test_url");
+    const auto actual_response = request_get("http://0.0.0.0:1001", "/test_url");
 
     EXPECT_EQ(status_codes::OK, actual_response.status_code());
     EXPECT_EQ(expect_response, actual_response.extract_string().get());
@@ -52,11 +52,11 @@ TEST(RestServer, test_post_p)
 {
     const std::string expect_response("response of test_post");
 
-    RestServer server(U("http://0.0.0.0:1234"));
+    RestServer server(U("http://0.0.0.0:1002"));
     server.OnPost("/test_url", [&](auto req) { return expect_response; });
     server.Run();
 
-    const auto actual_response = request_post("http://0.0.0.0:1234", "/test_url");
+    const auto actual_response = request_post("http://0.0.0.0:1002", "/test_url");
 
     EXPECT_EQ(status_codes::OK, actual_response.status_code());
     EXPECT_EQ(expect_response, actual_response.extract_string().get());
@@ -64,11 +64,11 @@ TEST(RestServer, test_post_p)
 
 TEST(RestServer, test_no_url)
 {
-    RestServer server(U("http://0.0.0.0:1234"));
+    RestServer server(U("http://0.0.0.0:1003"));
     server.Run();
 
-    const auto get_response = request_get("http://0.0.0.0:1234", "/no_url");
-    const auto post_response = request_post("http://0.0.0.0:1234", "/no_url");
+    const auto get_response = request_get("http://0.0.0.0:1003", "/no_url");
+    const auto post_response = request_post("http://0.0.0.0:1003", "/no_url");
 
     EXPECT_EQ(status_codes::NotFound, get_response.status_code());
     EXPECT_EQ(status_codes::NotFound, post_response.status_code());
@@ -79,27 +79,27 @@ TEST(RestServer, url_distribute)
     const std::string expect_response1("response 1");
     const std::string expect_response2("response 2");
 
-    RestServer server(U("http://0.0.0.0:1234"));
+    RestServer server(U("http://0.0.0.0:1004"));
     server.OnGet("/url1", [&](auto req) { return expect_response1; });
     server.OnGet("/url2", [&](auto req) { return expect_response2; });
     server.Run();
 
-    const auto actual_response1 = request_get("http://0.0.0.0:1234", "/url1");
+    const auto actual_response1 = request_get("http://0.0.0.0:1004", "/url1");
     EXPECT_EQ(status_codes::OK, actual_response1.status_code());
     EXPECT_EQ(expect_response1, actual_response1.extract_string().get());
 
-    const auto actual_response2 = request_get("http://0.0.0.0:1234", "/url2");
+    const auto actual_response2 = request_get("http://0.0.0.0:1004", "/url2");
     EXPECT_EQ(status_codes::OK, actual_response2.status_code());
     EXPECT_EQ(expect_response2, actual_response2.extract_string().get());
 }
 
 TEST(RestServer, throws_exception)
 {
-    RestServer server(U("http://0.0.0.0:1234"));
+    RestServer server(U("http://0.0.0.0:1005"));
     server.OnGet("/test_url", [&](auto req) -> rest::core::Response { throw std::exception(); });
     server.Run();
 
-    const auto actual_response = request_get("http://0.0.0.0:1234", "/test_url");
+    const auto actual_response = request_get("http://0.0.0.0:1005", "/test_url");
 
     EXPECT_EQ(status_codes::InternalError, actual_response.status_code());
 }
@@ -108,7 +108,7 @@ TEST(RestServer, exception_tolerant)
 {
     const std::string expect_response("good response after exception");
 
-    RestServer server(U("http://0.0.0.0:1234"));
+    RestServer server(U("http://0.0.0.0:1006"));
     static int call_count = 0;
     server.OnGet("/test_url", [&](auto req) {
         ++call_count;
@@ -118,11 +118,11 @@ TEST(RestServer, exception_tolerant)
     });
     server.Run();
 
-    const auto actual_response1 = request_get("http://0.0.0.0:1234", "/test_url");
+    const auto actual_response1 = request_get("http://0.0.0.0:1006", "/test_url");
     EXPECT_EQ(status_codes::InternalError, actual_response1.status_code());
     EXPECT_EQ(1, call_count);
 
-    const auto actual_response2 = request_get("http://0.0.0.0:1234", "/test_url");
+    const auto actual_response2 = request_get("http://0.0.0.0:1006", "/test_url");
     EXPECT_EQ(status_codes::OK, actual_response2.status_code());
     EXPECT_EQ(expect_response, actual_response2.extract_string().get());
     EXPECT_EQ(2, call_count);
